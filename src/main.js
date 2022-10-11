281b34f2a605b0f9f14506efb284417e7b2931a5
Anil <karnamanil2000@gmail.com>
2022-10-11 17:55:54 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import '@progress/kendo-theme-default/dist/all.css'
+
+import { GridComponent } from '@progress/kendo-vue-grid'
+
 
 Vue.config.productionTip = false
+
+Vue.component('GridComponent', GridComponent)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
